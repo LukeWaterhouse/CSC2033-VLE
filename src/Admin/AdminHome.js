4c4d1875ab9592174fa55eb0392473651f2d850d
b9021022 <h.j.clifford1@newcastle.ac.uk>
2020-12-16 21:51:59 +0000

    Styled the navigation bar in student

---
+++ src/Admin/AdminHome.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class AdminHome extends React.Component {
+    render() {
+        return (
+            <div className="AdminHome">
+                <p>
+                    Nav Bar Was 'Ere <br/>
+                    'Name' Welcome to the Admin Home
+                </p>
+            </div>
+        )
+    }
+}
+
+export default AdminHome
