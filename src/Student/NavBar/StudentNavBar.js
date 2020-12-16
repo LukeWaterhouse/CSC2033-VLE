dcd3f3c5b133f0c20c194d44261681f10e9b42b3
Luke <b9058369@newcastle.ac.uk>
2020-12-16 18:37:26 +0000

    started adding navbar Files and code

---
+++ src/Student/NavBar/StudentNavBar.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class StudentNavBar extends Component {
+    render() {
+        return(
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo" >React</h1>
+                <div className="menu-icon">
+
+                </div>
+                <ul>
+                    <li>
+                    </li>
+                </ul>
+
+            </nav>
+        )
+    }
+}
