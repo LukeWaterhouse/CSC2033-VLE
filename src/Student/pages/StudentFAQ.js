1683bb498125cdd94327cce85d4d27ea0cab0b89
Luke <b9058369@newcastle.ac.uk>
2020-12-22 14:08:17 +0000

    added css directories and started on the FAQ

--- src/Student/pages/StudentFAQ.js
+++ src/Student/pages/StudentFAQ.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import StudentNavBar from "../NavBar/StudentNavBar";
+import cssFiles from "../css-files/disccusionBoard.css"
 
 
 class StudentFAQ extends React.Component {
@@ -9,6 +10,12 @@ class StudentFAQ extends React.Component {
             <div>
                 <StudentNavBar/>
                 <h2>FAQ</h2>
+                <div className="panel panel-default post-editor">
+                    <div className="panel-body">
+                        Hello World
+
+                    </div>
+                </div>
             </div>
         );
     }
