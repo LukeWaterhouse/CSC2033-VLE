ce0866f6ebd8c3ba030f861f3d9716b1982ed894
Luke <b9058369@newcastle.ac.uk>
2020-12-29 21:16:35 +0000

    added a date timestamp for each message and cleaned up some bugs

--- src/Student/pages/StudentFAQ.js
+++ src/Student/pages/StudentFAQ.js
@@ -1,24 +1,18 @@
 import React, {useState} from 'react'
-
 import StudentNavBar from "../NavBar/StudentNavBar";
 import 'bootstrap/dist/css/bootstrap.css'
 import 'popper.js'
 import '../css-files/disccusionBoard.css'
 import {useCollectionData} from 'react-firebase-hooks/firestore'
 import 'firebase/firestore'
+import firebase from "firebase";
 import {db,auth} from '../../firebase'
 global.jQuery = require('jquery');
 require('bootstrap');
 
 
 
-
-
-
-
-
 function ChatRoom(){
-
     const messagesRef = db.collection('messages')
     const query = messagesRef.orderBy('createdAt').limit(25)
 
@@ -26,13 +20,11 @@ function ChatRoom(){
     const [formValue,setFormValue] = useState('')
 
     const sendMessage = async(e) => {
-        const today = new Date()
         e.preventDefault()
         await messagesRef.add({
             text: formValue,
-            createdAt: today.toLocaleDateString()+" "+ today.toLocaleTimeString()
+            createdAt: firebase.firestore.FieldValue.serverTimestamp()
         })
-
         setFormValue('')
     }
 
@@ -40,10 +32,8 @@ function ChatRoom(){
     return(
         <>
         <div>
-
             {messages ?.map(message => <ChatMessage key={message.id} message={message}/>)}
         </div>
-
             <div className="card card-body post-editor">
 
                 <div className="panel-body">
@@ -51,34 +41,30 @@ function ChatRoom(){
                     <form onSubmit={sendMessage}>
                         <textarea className="form-control post-editor-input" value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
                         <button  className="btn btn-success post-editor-button" type="submit">Post</button>
-
-
                     </form>
-
-
                 </div>
-
-
             </div>
-
-
-
-
             </>
-
-
-
-
     )
 }
 
-
-
 function ChatMessage({message}){
     const {text,createdAt} = message
-    return <p className="card card-body post-editor">{text} {createdAt}</p>
-}
+    const date = createdAt && createdAt.toDate() // checks if createdAt exists and if so turns it into JS date format
+    let output = "DataBase Error!";
+    if (date!=null){
+        const month = date.getUTCMonth()
+        const day = date.getUTCDate();
+        const year = date.getUTCFullYear();
+        const time = date.getUTCHours()+":"+date.getUTCMinutes()
+        output = year+"/"+month+"/"+day+" "+time
 
+    }
+
+
+
+    return <p className="card card-body post-editor"><p className="text-black-50">{output}</p>{text} </p>
+}
 
 function StudentFAQ(){
 
