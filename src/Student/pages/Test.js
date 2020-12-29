42f402baae92007736ecb7d4288447042a765ad8
Luke <b9058369@newcastle.ac.uk>
2020-12-29 18:44:37 +0000

    Completed a working draft of the discussion board, going to continue to improve it and probably create a way to add threads

--- src/Student/pages/Test.js
+++ src/Student/pages/Test.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {db,auth} from '../../firebase'
-import StudentNavBar from "../../Student/NavBar/StudentNavBar";
 
 class StudentHome extends React.Component {
     state = {
