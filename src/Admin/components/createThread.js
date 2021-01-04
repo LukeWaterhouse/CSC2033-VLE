04fa246d69e9572dfa913e45d845369abe2b5a63
Luke <b9058369@newcastle.ac.uk>
2021-01-04 13:59:39 +0000

    The Create Forum feature is now functional, can be found in the admin Forums

--- src/Admin/components/createThread.js
+++ src/Admin/components/createThread.js
@@ -1,25 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
 import {db} from "../../firebase";
-import {useState} from "react/cjs/react.production.min";
 
 function CreateThread(){
 
-    const threadRef = db.collection('Threads').doc("newThread").collection('messages')
     const [formValue,setFormValue] = useState('')
+    const threadRef = db.collection('Threads')
+
     const createThread = async (e) => {
         e.preventDefault()
-        await threadRef.add({
-
+        await threadRef.doc(formValue).set({
+            Title: formValue
         })
-
 
     }
 
     return(
         <div>
             Create Thread
-
-            <form onClick={createThread}>
+            <form onSubmit={createThread}>
                 <textarea value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
                 <button type="submit">Create</button>
             </form>
