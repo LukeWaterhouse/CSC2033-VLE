689c5cbbd836299a77c5100d124a6dda27878847
b9021022 <h.j.clifford1@newcastle.ac.uk>
2021-01-12 10:43:50 +0000

    Added list of project for student deadlines

--- src/Student/pages/StudentDeadlines.js
+++ src/Student/pages/StudentDeadlines.js
@@ -1,5 +1,6 @@
 import React from "react";
 import StudentNavBar from "../NavBar/StudentNavBar";
+import data from "../../DataAnalysis/Data";
 
 class StudentDeadlines extends React.Component {
   render() {
@@ -7,6 +8,16 @@ class StudentDeadlines extends React.Component {
       <div>
         <StudentNavBar />
         <h2>Student Deadlines</h2>
+          <ul className="AssignmentList">
+              {data.map((item, index) => {
+                  return(
+                      <li key={index} className="Assignment">
+                          <h5> {item.Title} </h5>
+                          Deadline date goes here
+                      </li>
+                  )
+              })}
+          </ul>
       </div>
     );
   }
