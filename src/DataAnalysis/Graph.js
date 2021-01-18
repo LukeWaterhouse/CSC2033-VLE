49f2c23d949bd8d51e05ec559e15d3d3c84fa949
b9021022 <h.j.clifford1@newcastle.ac.uk>
2021-01-18 14:53:00 +0000

    Graph's to the max update, lots of legends stuff and data upgrades as well as new UQ LQ and median code

--- src/DataAnalysis/Graph.js
+++ src/DataAnalysis/Graph.js
@@ -1,24 +1,40 @@
 import React, { Component } from "react";
-import {
-    LineChart,
-    Line,
-    CartesianGrid,
-    XAxis,
-    YAxis,
-    Tooltip,
-    Label,
-    ResponsiveContainer,
-} from "recharts";
-import data from "./Data";
 import CSS from "./GraphElement.css";
+import data from "./Data";
+import cumulativeData from "./TemporaryData";
+import CumulativeFrequencyGraph from "./CumulativeFrequency";
+import Format from "./CumulativeDataFormatting";
+
+var newData = Format(cumulativeData);
+
+let labels = [
+    { key: "Median", color: "#3700B3"},
+    { key: "Mean", color: "#03DAC6"},
+    { key: "UQ", color: "#FFC61A"},
+    { key: "LQ", color: "#00E600"}
+];
+
+let cumulativeLabels = [
+    { key: "Median", color: "#3700B3"},
+    { key: "UQ", color: "#FFC61A"},
+    { key: "LQ", color: "#00E600"}
+];
 
 class Graph extends Component{
     render(){
         return(
             <div className="Panel">
-                <BarChart/>
+                <ul className="GraphEditor">
+                    <li> <button onClick="Overall()"> Overall </button> </li>
+                    <li> <button onClick="Module()"> Test Module </button></li>
+                </ul>
+
+                <CumulativeFrequencyGraph
+                    title="Filler Data"
+                    data={newData}
+                    labels={cumulativeLabels}/>
                 <ul className="InfoSet">
-                    <h3>Progress</h3>
+                    <h3>Progress Report</h3>
                     <li>I must write this out for text size</li>
                 </ul>
                 <ul className="InfoSet">
@@ -31,7 +47,7 @@ class Graph extends Component{
                             <li key={index} className="Assignment">
                                 <h5> {item.Title} </h5>
                                 <p>
-                                    Score: {item.IndividualMark} Median: {item.Median} Mean: {item.Mean}
+                                    Mark: {item.Mark} Median: {item.Median} Mean: {item.Mean}
                                 </p>
                             </li>
                         )
@@ -41,5 +57,6 @@ class Graph extends Component{
         )
     }
 }
+
 
 export default Graph
